c99821806fb87b221457dfcf54c2cc67becaae65
AndrewSC208 <meilingandrew@gmail.com>
2018-08-23 07:09:05 -0700

    Finished login form, now working on hooking up firebase login, and the final steps of the login flow

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,19 +1,55 @@
 import React, {Component} from "react";
+import firebase from "firebase";
 import {
     Button,
     Card,
-    CardSection
+    CardSection,
+    Input
 } from "./common";
 
 class LoginForm extends Component {
+    state = {
+        email: '',
+        password: ''
+    };
+
+    onButtonPress() {
+        const {
+            email,
+            password
+        } = this.state;
+
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
+
     render() {
+        const { 
+            email, 
+            password 
+        } = this.state;
+
         return (
             <Card>
-                <CardSection />
-                <CardSection />
-                
                 <CardSection>
-                    <Button>Login</Button>
+                    <Input
+                        onChangeText={e => this.setState({e})} 
+                        label={'Email'}
+                        placeholder={'user@gmail.com'}
+                        value={email} />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        onChangeText={pw => this.setState({pw})}
+                        label={'Password'}
+                        placeholder={'password'}
+                        value={password} 
+                        secureTextEntry={true}/>
+                </CardSection>
+
+                <CardSection>
+                    <Button
+                    onPress={this.onButtonPress.bind(this)}>Login</Button>
                 </CardSection>
             </Card>
         )
